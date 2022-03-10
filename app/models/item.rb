d5b40599bb1b6be4eaf65b88be1c5a4a6a3b1dd4
hiroyuki0305 <98807620+hiroyuki0305@users.noreply.github.com>
2022-03-10 20:53:51 +0900

    エラーハンドリングの一部修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
   validates :image, :name, :info
   validates :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_deliver_id,
   numericality: { other_than: 1 ,message: "can't be blank"}
-  validates :price, format: { with: /\A[0-9]+\z/ }, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"} 
+  validates :price, format: { with: /\A[0-9]+\z/,message: "Price is invalid. Input half-width characters" }, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Price is out of setting range"} 
 
   end
 
