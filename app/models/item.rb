78b46c095d154741a9306729a73762f58f27ab9e
hiroyuki0305 <98807620+hiroyuki0305@users.noreply.github.com>
2022-03-10 20:12:14 +0900

    価格の半角数字のみ制限

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,8 +10,9 @@ class Item < ApplicationRecord
   validates :name, :info, :price
   validates :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_deliver_id,
   numericality: { other_than: 1 ,message: "can't be blank"}
-  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"} 
-end
+  validates :price, format: { with: /\A[0-9]+\z/ }, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"} 
+
+  end
 
   belongs_to :user
   has_one_attached :image
