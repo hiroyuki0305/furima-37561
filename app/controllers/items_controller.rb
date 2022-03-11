e3fadea2d1e84e53594cc66a77232a13653d29e9
hiroyuki0305 <98807620+hiroyuki0305@users.noreply.github.com>
2022-03-11 15:30:25 +0900

    単体テストコードまで

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
 
   def index
-    #@items = Item.order("created_at DESC")
   end
 
   def new
@@ -21,7 +20,7 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :name, :info,:category_id, :sales_status_id,
-      :shipping_fee_status_id, :prefecture_id, :scheduled_deliver_id, :price)
+      :shipping_fee_status_id, :prefecture_id, :scheduled_deliver_id, :price, :image)
       .merge(user_id: current_user.id)
   end
 
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name                     { '商品名' }
+    info                     { '商品の説明' }
+    category_id              { 2 }
+    sales_status_id          { 2 }
+    shipping_fee_status_id   { 2 }
+    prefecture_id            { 2 } 
+    scheduled_deliver_id     { 2 }
+    price                    { '300' }
+    association :user
+  
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
