cb48f642e8a7a88e2c13aca90dda31fba2487e34
hiroyuki0305 <98807620+hiroyuki0305@users.noreply.github.com>
2022-03-18 10:21:13 +0900

    一部修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -5,6 +5,6 @@ window.addEventListener('load', () => {
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
     const addProfitDom = document.getElementById("profit");
-    addProfitDom.innerHTML = Math.floor(inputValue - (inputValue * 0.1));         
+    addProfitDom.innerHTML = Math.round(inputValue - (inputValue * 0.1));         
   });
 });
